197bfa87e940d056ca7610aa75a4f3d4593e926e
ImSerdar <86382278+ImSerdar@users.noreply.github.com>
2025-10-29 11:24:58 -0700

    Add next.config.js for Webflow Cloud configuration

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// This file is required by Webflow Cloud
+// It sets your base path to match your Mount Path.
+// Your Mount Path is '/home'
+module.exports = {
+  basePath: '/home',
+  assetPrefix: '/home',
+  output: 'standalone',
+  reactStrictMode: true
+};
