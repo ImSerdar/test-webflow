3b7bd6759a101a98077ab4183a5008bdf756c108
ImSerdar <86382278+ImSerdar@users.noreply.github.com>
2025-10-29 11:25:15 -0700

    Add open-next.config.ts for Webflow Cloud setup

---
+++ open-next.config.ts
@@ -0,0 +1,7 @@
+// This file is required by Webflow Cloud
+import type { NextConfig } from "next";
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({
+  // options
+});
